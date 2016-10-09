f69b8d32a3508b6790e34ce4419c7e1c052cdedb
Matthew J. Berger <matthewberger@nevada.unr.edu>
2016-10-09 16:25:50 -0700

    Initial commit.

---
+++ Assets/Scripts/Beard.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Beard : MonoBehaviour
+{
+    public static Beard S;
+    private Rigidbody2D rigidBody;
+    private ParticleSystem ps;
+
+    // Use this for initialization
+    void Start ()
+    {
+        S = this;
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = Vector2.right;
+        ps = GetComponentInChildren<ParticleSystem>();
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape)) Application.Quit();
+        
+        //if ( Input.GetTouch(0).tapCount > 0 )
+        foreach(var i in Input.touches)
+        {
+            if (i.phase == TouchPhase.Began)
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, 2);
+            }
+        }
+
+        if (Math.Abs(rigidBody.position.y) > ((GetComponent<Renderer>().bounds.size.y/2) + 1) ) Die();
+    }
+
+    void Die()
+    {
+        transform.position = Vector2.zero;
+        rigidBody.velocity = Vector2.right;
+        var pstmp = Instantiate(ps);
+        Destroy(ps);
+        ps = pstmp;
+        ps.transform.parent = transform;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Fatal")
+        {
+            Die();
+        }
+    }
+}
---
+++ Assets/Scripts/MainCam.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCam : MonoBehaviour {
+
+    public static MainCam S;
+
+    // Use this for initialization
+    void Start () {
+        S = this;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        Vector3 position = new Vector3(gameObject.transform.position.x,0,gameObject.transform.position.z);
+        gameObject.transform.position = position;
+    }
+}
